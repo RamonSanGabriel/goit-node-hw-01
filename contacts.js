6138dfa03034cd50619ef673e8f6a82de503b9f3
RamonSanGabriel <jeshero11@gmail.com>
2024-09-23 23:19:13 +0800

    update

---
+++ contacts.js
@@ -0,0 +1,56 @@
+import fs from 'fs/promises';
+// const fs = require('fs/promises');
+import path from 'path';
+// import { nanoid } from 'nanoid';
+
+export const contactsPath = path.join('db', 'contacts.json');
+
+// module.exports = { contactsPath };
+
+const listContacts = async () => {
+  try {
+    const contacts = await fs.readFile(contactsPath);
+    return JSON.parse(contacts);
+  } catch (error) {
+    console.error('Error reading contacts:', error.message);
+  }
+};
+
+const getContactById = async (contactId) => {
+  try {
+    const contacts = await listContacts();
+    const result = contacts.find((contacts) => contact.id === contactId);
+    return result || null;
+  } catch (error) {
+    console.error('Error reading the contacts by id!', error.message);
+  }
+};
+
+const removeContact = async (contactId) => {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((item) => item.id === contactId);
+    if (index === -1) {
+      return null;
+    }
+    const deleteContact = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return deleteContact;
+  } catch (error) {
+    console.error('Error removing contact:', error.message);
+  }
+};
+
+const addContact = async ({ name, email, phone }) => {
+  try {
+    const contacts = await listContacts();
+    const newContact = { id: nanoid(), name, email, phone };
+    const allContacts = [...contacts, newContact];
+    await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+    return newContact;
+  } catch (error) {
+    console.error('Error adding new contact:', error.message);
+  }
+};
+
+export { listContacts, getContactById, removeContact, addContact };
---
+++ index.js
@@ -0,0 +1,13 @@
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} from './contacts.js';
+import { contactsPath } from './contacts.js';
+
+listContacts();
+getContactById();
+removeContact();
+// console.log(addContact(name, email, phone));
+// console.log(contactsPath);
